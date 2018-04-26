5847c3396952e58c418bac5baf34d91b16ead560
OPISTO\ari.punavuori <ari.punavuori@gmail.com>
2018-04-26 15:08:50 +0300

    Ari's Character Movement Start, Sprite Assets

---
+++ MrDriller/Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour {
+
+
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            transform.position += Vector3.right; 
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            transform.position += Vector3.left;
+        }
+    }
+}
