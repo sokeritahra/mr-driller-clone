7897e7d2246096b235f09133094c462b340c7ef0
OPISTO\ari.punavuori <ari.punavuori@gmail.com>
2018-04-27 10:35:08 +0300

    Character movement final v0.1

--- MrDriller/Assets/Scripts/PlayerCharacter.cs
+++ MrDriller/Assets/Scripts/PlayerCharacter.cs
@@ -5,10 +5,9 @@ using UnityEngine;
 public class PlayerCharacter : MonoBehaviour {
     Rigidbody2D rb;
     public float speed;
-    // enum playerModes[Up, Down, Left, Right, Falling]; // Modes for the sprites
-    void Start()
-    {
-       rb = GetComponent<Rigidbody2D>();
+    //enum playerModes[Up, Down, Left, Right, Falling]; // Future modes for the sprites
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
     }
 
 
