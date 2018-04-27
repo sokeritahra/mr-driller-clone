e9d6401ca1693db8c0f8ef1605d33d026ec193e8
OPISTO\ari.punavuori <ari.punavuori@gmail.com>
2018-04-27 11:49:25 +0300

    MrDriller has "Modes" now (Up, Down, Left and Right). (Changed the sprite folder name to simpler one)

--- MrDriller/Assets/Scripts/PlayerCharacter.cs
+++ MrDriller/Assets/Scripts/PlayerCharacter.cs
@@ -1,18 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+enum PlayerMode { Up, Down, Left, Right, Falling }; // Mmodes for the sprites
 
 public class PlayerCharacter : MonoBehaviour {
     Rigidbody2D rb;
+    SpriteRenderer sr;
+    PlayerMode pm;
     public float speed;
-    //enum playerModes[Up, Down, Left, Right, Falling]; // Future modes for the sprites
+
+    float vertical;
+    float horizontal;
+
     void Start() {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
 
     void FixedUpdate () {
-        float move = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(move * speed, rb.velocity.y);
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+
+        if (Mathf.Abs(horizontal) < Mathf.Abs(vertical)) {
+            if (vertical < 0) {
+                pm = PlayerMode.Down;
+            }
+            if (vertical > 0) {
+                pm = PlayerMode.Up;
+            }
+        } else {
+            if (horizontal < 0) {
+                pm = PlayerMode.Right;
+            }
+            if (horizontal > 0) {
+                pm = PlayerMode.Left;
+            }
+        }
+        
+        //if (pm == PlayerMode.Down) {
+            
+        //}
+        //if (pm == PlayerMode.Up) {
+
+        //}
+        //if (pm == PlayerMode.Left) {
+
+        //}
+        //if (pm == PlayerMode.Right) {
+
+        //}
     }
 }
