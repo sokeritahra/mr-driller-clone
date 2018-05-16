ed6a378110c9c0c78fa1234ef7cb30b8ee8083f4
OPISTO\ari.punavuori <ari.punavuori@gmail.com>
2018-05-16 14:06:57 +0300

    New Blocks On The Kid

---
+++ MrDriller/Assets/BlockSpriteChanger.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpriteChanger : MonoBehaviour {
+    public SpriteRenderer topLeft;
+    public SpriteRenderer topRight;
+    public SpriteRenderer bottomLeft;
+    public SpriteRenderer bottomRight;
+    public Sprite cornerOut;
+    public Sprite cornerIn;
+    public Sprite edge;
+
+    public bool sameAbove;
+    public bool sameLeft;
+    public bool sameBelow;
+    public bool sameRight;
+
+    List <SpriteRenderer> blockTiles;
+
+    void Awake() {
+
+        topLeft = transform.Find("TopLeft").GetComponent<SpriteRenderer>();
+        topRight = transform.Find("TopRight").GetComponent<SpriteRenderer>();
+        bottomLeft = transform.Find("BottomLeft").GetComponent<SpriteRenderer>();
+        bottomRight = transform.Find("BottomRight").GetComponent<SpriteRenderer>();
+
+        blockTiles = new List<SpriteRenderer> {bottomRight, topRight, bottomLeft, topLeft};
+    }
+
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            SpriteUpdate();
+        }
+	}
+
+    public void SpriteUpdate() {
+        for (int cornerID = 0; cornerID < 4; cornerID++) {
+            bool checkUp = (cornerID & 1) != 0;
+            print(checkUp);
+            bool checkLeft = (cornerID & 2) != 0;
+            print(checkLeft);
+            bool sameAboveOrBelow = checkUp ? sameAbove : sameBelow;
+            print(sameAboveOrBelow);
+            bool sameLeftOrRight = checkLeft ? sameLeft : sameRight;
+            print(sameLeftOrRight);
+            int index = cornerID + (sameAboveOrBelow ? 4 : 0) + (sameLeftOrRight ? 8 : 0);
+
+            // cornerOut cornerIn edge ? 
+            if (sameAboveOrBelow && sameLeftOrRight) {
+                blockTiles[cornerID].sprite = cornerIn;
+            } else if (sameAboveOrBelow && !sameLeftOrRight) {
+                blockTiles[cornerID].sprite = edge;
+            } else if (!sameAboveOrBelow && sameLeftOrRight) {
+                blockTiles[cornerID].sprite = edge;
+            } else {
+                blockTiles[cornerID].sprite = cornerOut;
+            }
+
+        }
+
+    }
+}
