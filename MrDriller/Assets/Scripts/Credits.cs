56faadccf07537d09f738a331c12e3f066c62448
OPISTO\ari.punavuori <ari.punavuori@gmail.com>
2018-06-06 16:45:02 +0300

    Creditsitsitisti

---
+++ MrDriller/Assets/Scripts/Credits.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.y < 9.5f) {
+            transform.position = transform.position + (Vector3.up * 0.01f);
+        }
+	}
+}
